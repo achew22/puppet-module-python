c52882ffeeed4ee98b2b2ac6950c96a951209bed
Eivind Uggedal <eivind@uggedal.com>
2011-04-16 10:41:30 +0200

    Namespaced class name and colon before arguments trips Puppet up.

--- manifests/venv.pp
+++ manifests/venv.pp
@@ -1,6 +1,6 @@
 class python::venv($ensure=present, $version=latest) {
 
-  class { python::dev: version => $version }
+  class { "python::dev": version => $version }
 
   package { $python_dev_package:
     ensure => $ensure,
